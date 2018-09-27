07224ede42153ec616698800064b807586c25a02
Jimmy Depetro <jimmy.depetro@evopayments.com>
2018-09-27 17:43:47 -0600

    installed and configured flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_moment import Moment
 
 from config import Config
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
