6a37e4096dcad6479eb41ba60eb4617072585d36
Jimmy Depetro <jimmy.depetro@evopayments.com>
2018-09-27 17:21:18 -0600

    created user login model and form

--- app/models.py
+++ app/models.py
@@ -1,11 +1,15 @@
 from datetime import datetime
-
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
 
-from app import db
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     email = db.Column(db.String(120), index=True, unique=True)
