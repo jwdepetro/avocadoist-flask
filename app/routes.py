7b6d23be302f24371c4231b1928677a9bb12bd54
Jimmy <jwdepetro@depetro-imac.lan>
2018-10-18 19:22:05 -0600

    adding pagination to home page

--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,9 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(
         Post.timestamp.desc()).paginate(page, 50, False)
-    return render_template('home.html', posts=posts.items)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('home.html', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
