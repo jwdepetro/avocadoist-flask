3faaddc5704ae73fb618e7e82f2e4635a8c09204
Jimmy <jwdepetro@macbooks-mbp.lan>
2018-10-23 20:20:58 -0600

    gunicorn reloads for dev environments

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,10 @@ python manage.py db init
 python manage.py db migrate
 python manage.py db upgrade
 
-gunicorn --bind 0.0.0.0:5090 app:app
+if [ "$FLASK_ENV" = "development" ]; then
+    echo "starting gunicorn for development"
+    gunicorn --bind 0.0.0.0:5090 --reload app:app
+else
+    echo "starting gunicorn"
+    gunicorn --bind 0.0.0.0:5090 app:app
+fi
